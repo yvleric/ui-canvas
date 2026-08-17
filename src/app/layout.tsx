e0414c7338950849b86addfaf9ae43f6bd94a035
ericsinghh <singheric890@gmail.com>
2026-08-17 22:21:36 +0530

    root layout and base page components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { LayoutProps } from "@/types";
 import "./globals.css";
 
 const geistSans = Geist({
