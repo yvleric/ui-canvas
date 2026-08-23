95bf5537dcc3bfebbe3c3cf1f6d01b3f51b65632
ericsinghh <singheric890@gmail.com>
2026-08-23 12:48:14 +0530

    component with responsive layout and blur effect

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between items-center px-4 dm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70'>
+
+        </div>
+    )
+}
+
+export default Navbar
