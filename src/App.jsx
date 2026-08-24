c8a044ff7d531e0530c4c1ddc547038165f67daa
ericsinghh <singheric890@gmail.com>
2026-08-24 20:59:25 +0530

    layout with responsive Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-
+    <div className='dark:bg-black relative'>
+      <Navbar />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import assets from "../assets/assets";
 
-const Navbar = () => {
+const Navbar = ({ theme, setTheme }) => {
     return (
         <div className='flex justify-between items-center px-4 dm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70'>
+
+            <img src={theme === 'dark' ? assets.logo_dark : assets.logo}
+                className='w-32 sm:w-40' alt='' />
 
         </div>
     )
