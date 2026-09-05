27fa6f6cdab39074a5014d094912bf985be7bbcd
ericsinghh <singheric890@gmail.com>
2026-09-05 23:26:42 +0530

    component and implement Hero section with theme state management

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import assets from '../assets/assets';
 const Hero = () => {
     return (
         <div id='hero' className='flex flex-col items-center gap-6 py-20 px-4 sm:px-12 
