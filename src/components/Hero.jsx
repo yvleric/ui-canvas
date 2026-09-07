5729946043babb80ebb26fea7cc504197267d94d
ericsinghh <singheric890@gmail.com>
2026-09-07 23:31:01 +0530

    component with branding and call-to-action layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,20 @@ const Hero = () => {
                 <p className='text-xs font-medium'>Trusted by 10k+ people</p>
             </div>
 
-        </div>
+            <h1 className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium 
+            xl:leading-[95px] max-w-5xl'> Turning imagination into <span
+                    className='bg-gradient-to-r from-[#5044E5] to-[#4d8cea] bg-clip-text
+            text-transparent'>digital</span> impact.</h1>
+
+            <p className='text-sm sm:text-lg font-medium text-gray-500 dark:text-white/
+            75 max-w-4/5 sm:max-w-lg pb-3'>Creating meaningful connections and turning big ideas
+                into interactive digital experiences.</p>
+
+            <div className='relative'>
+                <img src={assets.hero_img} alt="" className='w-full max-w-6xl' />
+            </div>
+
+        </div >
     )
 }
 
