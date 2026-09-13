f627fe4372487fa416c668cb57bd342e9aab0fa3
ericsinghh <singheric890@gmail.com>
2026-09-13 23:16:03 +0530

    Add Services component to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import TrustedBy from './components/TrustedBy'
-
+import Services from './components/Services'
 
 const App = () => {
 
@@ -13,6 +13,7 @@ const App = () => {
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
       <TrustedBy />
+      <Services />
     </div>
   )
 }
