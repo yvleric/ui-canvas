7f0add25207e3ec778113089086e8a8e92dadca0
ericsinghh <singheric890@gmail.com>
2026-09-15 23:30:52 +0530

    Add OurWork component and update Services formatting

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -31,7 +31,7 @@ const Services = () => {
     return (
         <div id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
 
-            <img src={assets.bgImage2} alt="" className='absolute -top-110 -left-70 -z-1 dark:hidden'/>
+            <img src={assets.bgImage2} alt="" className='absolute -top-110 -left-70 -z-1 dark:hidden' />
 
             <Title title='How can we help?' desc='From strategy to execution, we craft digital solutions that drive real business' />
 
@@ -44,4 +44,4 @@ const Services = () => {
     )
 }
 
-export default Services
+export default Services
